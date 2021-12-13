0f851b806991bff473866eb24e2132254432bee5
Felipe Requena <42552805+lipegarcia946@users.noreply.github.com>
2021-12-13 09:50:44 -0300

    Novo sql
    subindo o novo sql

--- Documentação/Sql_CriaçãoGrupo1.sql
+++ Documentação/Sql_CriaçãoGrupo1.sql
@@ -6,18 +6,19 @@ CREATE TABLE `clientes` (
 	`cpfCliente` varchar(14) NOT NULL,
 	`telefoneCliente` varchar(14) NOT NULL,
 	`emailCliente` varchar(60) NOT NULL,
-	`clienteConta` INT NOT NULL,
+	`contaCliente` INT(255) NOT NULL,
 	PRIMARY KEY (`idCliente`)
 );
 
 CREATE TABLE `contas` (
 	`numConta` INT NOT NULL AUTO_INCREMENT,
+	`ClienteTitular` varchar(50) NOT NULL,
 	`agenciaConta` INT NOT NULL,
 	`TipoConta` INT NOT NULL,
-	`saldoConta` DOUBLE NOT NULL,
+	`saldoConta` FLOAT NOT NULL,
 	PRIMARY KEY (`numConta`)
 );
 
-ALTER TABLE `clientes` ADD CONSTRAINT `clientes_fk0` FOREIGN KEY (`clienteConta`) REFERENCES `contas`(`numConta`);
+ALTER TABLE `clientes` ADD CONSTRAINT `clientes_fk0` FOREIGN KEY (`contaCliente`) REFERENCES `contas`(`numConta`);
 
 
